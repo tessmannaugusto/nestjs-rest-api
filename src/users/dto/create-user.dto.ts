89600118871d5cda42ff6777204c3443a245a827
Augusto Yunes <augusto.yunes@accenture.com>
2025-01-29 20:35:06 -0300

    4 first lessons: controllers, providers, DTO's, validation and pipes

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsEnum(['INTERN', 'ENGINEER', 'ADMIN'])
+  role: 'INTER' | 'ENGINEER' | 'ADMIN';
+}
