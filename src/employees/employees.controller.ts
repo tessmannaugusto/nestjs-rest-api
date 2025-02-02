1e82ada041e2b78ff9bd7c7b2896757d925ee8d7
Augusto Yunes <augusto.yunes@accenture.com>
2025-02-02 13:23:14 -0300

    specific route rating limits and logger class

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -7,24 +7,32 @@ import {
   Param,
   Delete,
   Query,
+  Ip,
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
-import { Prisma } from '@prisma/client';
+import { Prisma, Role } from '@prisma/client';
+import { Throttle, SkipThrottle } from '@nestjs/throttler';
+import { MyLoggerService } from 'src/my-logger/my-logger.service';
 
+@SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
+  private readonly logger = new MyLoggerService(EmployeesController.name);
 
   @Post()
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
     return this.employeesService.create(createEmployeeDto);
   }
 
+  @SkipThrottle({ default: false })
   @Get()
-  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+  findAll(@Ip() ip: string, @Query('role') role?: Role) {
+    this.logger.log(`Request for ALL Employees \t ${ip}`);
     return this.employeesService.findAll(role);
   }
 
+  @Throttle({ second: { ttl: 1000, limit: 1 } })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.employeesService.findOne(+id);
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.useLogger(app.get(MyLoggerService)); // this applies to all logs of the server
   app.enableCors(); // add config options to limit access to certain domains
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
